97be052545a9b206711c3db7ca7a3eaebea50845
Pedro-lms <pedrodaluzmachado@gmail.com>
2021-11-03 20:08:21 -0300

    Initial and Basic Setup

---
+++ src/main/java/com/deliver/BackendDeliverApplication.java
@@ -0,0 +1,13 @@
+package com.deliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDeliverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deliver/BackendDeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
